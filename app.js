c39b6f680a7648f53accbd651ffd4acf36429543
Adarsh Kumar <adarshkumar245@gmail.com>
2023-01-05 07:06:06 +0530

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -8,14 +8,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const storeRouter = require("./routes/store"); //Import routes for "store" area of site
 
+const compression = require("compression");
+const helmet = require("helmet");
+
 var app = express();
 
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const fs = require("fs");
 require("dotenv/config");
-// const mongodb = "mongodb+srv://AdarshKumar19:Mniaki%4022yo@cluster0.ugiud.mongodb.net/lights_out?retryWrites=true&w=majority";
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const mongodb = process.env.MONGODB_URL || process.env.MONGO_DEV_URI;
+
+mongoose.connect(mongodb, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Mongodb connection error"));
 
@@ -23,6 +28,8 @@ db.on("error", console.error.bind(console, "Mongodb connection error"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression())
+app.use(helmet())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
