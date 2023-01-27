34ddb15367f3e10eff2a8294e2c2e886e3f6e2dc
Adarsh Kumar <adarshkumar245@gmail.com>
2023-01-27 07:23:21 +0530

    update package.json and views

--- app.js
+++ app.js
@@ -29,14 +29,13 @@ db.on("error", console.error.bind(console, "Mongodb connection error"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(compression())
-app.use(helmet())
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
